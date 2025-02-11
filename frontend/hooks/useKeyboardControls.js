dd9f1cfbaab073342164692222f131405cf262af
Amarasena <amarasenaisuru@gmail.com>
2025-02-11 11:54:30 +0530

    fixing the java socket

--- frontend/hooks/useKeyboardControls.js
+++ frontend/hooks/useKeyboardControls.js
@@ -13,46 +13,89 @@ export function useKeyboardControls() {
   const { camera } = useThree()
 
   useEffect(() => {
+    const socket = new WebSocket('ws://192.168.56.1:12345')
+
+    socket.onopen = () => {
+      console.log('Connected to the server')
+    }
+
+    socket.onmessage = (event) => {
+      const message = JSON.parse(event.data)
+      console.log('Received from server:', message)
+
+      // Example: Handling player positions from the server
+      if (message.type === 'playerPosition') {
+        camera.position.set(message.x, message.y, message.z)
+      }
+    }
+
     const handleKeyDown = (event) => {
+      let newMovement = { ...movement }
+      let shouldSend = false
+
       switch (event.code) {
         case 'KeyW':
-          setMovement((m) => ({ ...m, forward: true }))
+          newMovement.forward = true
+          shouldSend = true
           break
         case 'KeyS':
-          setMovement((m) => ({ ...m, backward: true }))
+          newMovement.backward = true
+          shouldSend = true
           break
         case 'KeyA':
-          setMovement((m) => ({ ...m, left: true }))
+          newMovement.left = true
+          shouldSend = true
           break
         case 'KeyD':
-          setMovement((m) => ({ ...m, right: true }))
+          newMovement.right = true
+          shouldSend = true
           break
+      }
+
+      if (shouldSend) {
+        setMovement(newMovement)
+        socket.send(JSON.stringify({ type: 'keydown', key: event.code }))
       }
     }
 
     const handleKeyUp = (event) => {
+      let newMovement = { ...movement }
+      let shouldSend = false
+
       switch (event.code) {
         case 'KeyW':
-          setMovement((m) => ({ ...m, forward: false }))
+          newMovement.forward = false
+          shouldSend = true
           break
         case 'KeyS':
-          setMovement((m) => ({ ...m, backward: false }))
+          newMovement.backward = false
+          shouldSend = true
           break
         case 'KeyA':
-          setMovement((m) => ({ ...m, left: false }))
+          newMovement.left = false
+          shouldSend = true
           break
         case 'KeyD':
-          setMovement((m) => ({ ...m, right: false }))
+          newMovement.right = false
+          shouldSend = true
           break
+      }
+
+      if (shouldSend) {
+        setMovement(newMovement)
+        socket.send(JSON.stringify({ type: 'keyup', key: event.code }))
       }
     }
 
     const handleMouseMove = (event) => {
       if (document.pointerLockElement) {
-        setRotation((r) => ({
-          x: r.x - event.movementY * 0.002,
-          y: r.y - event.movementX * 0.002,
-        }))
+        const newRotation = {
+          x: rotation.x - event.movementY * 0.002,
+          y: rotation.y - event.movementX * 0.002,
+        }
+
+        setRotation(newRotation)
+        socket.send(JSON.stringify({ type: 'mouseMove', rotation: newRotation }))
       }
     }
 
@@ -64,8 +107,9 @@ export function useKeyboardControls() {
       window.removeEventListener('keydown', handleKeyDown)
       window.removeEventListener('keyup', handleKeyUp)
       window.removeEventListener('mousemove', handleMouseMove)
+      socket.close()
     }
-  }, [])
+  }, [movement, rotation, camera]) // Ensure dependencies are handled properly
 
   useEffect(() => {
     camera.rotation.x = rotation.x
@@ -74,4 +118,3 @@ export function useKeyboardControls() {
 
   return movement
 }
-
